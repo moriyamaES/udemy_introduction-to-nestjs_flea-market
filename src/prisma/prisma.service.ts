63ff7ff32a4d326f3df078e26d1cd71ee58bdbc7
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-13 21:56:41 +0900

    保存

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    // This method is called when the module is initialized
+    // It is a good place to connect to the database
+    // and perform any necessary setup
+    // For example, you can run migrations or seed the database
+    // await this.$connect();
+    // await this.$executeRaw`SELECT 1`;
+    await this.$connect();
+  }
+}
