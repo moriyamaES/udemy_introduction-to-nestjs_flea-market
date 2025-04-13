364be69588e763819d3782fca01ecad282d82272
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-14 00:02:51 +0900

    ユーザ作成機能の追加Part1

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,33 @@
+import { UserStatus } from 'generated/prisma';
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { MinLength } from 'class-validator';
+import { IsStrongPassword } from 'class-validator';
+
+// import { IsEmail, IsEnum, IsNotEmpty, IsString, MinLength, IsStrongPassword } from 'class-validator';
+
+export class CreateUserDto {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsString()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsNotEmpty()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @MinLength(20)
+  name: string;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsEmail()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsNotEmpty()
+  email: string;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+  })
+  password: string;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsEnum(UserStatus)
+  status: UserStatus;
+}
