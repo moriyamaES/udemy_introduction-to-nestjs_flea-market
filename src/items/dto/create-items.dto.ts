6ed6b142015b3e796e0cbacb0c905b2020e9636e
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-13 20:36:11 +0900

    Class Validatorを利用したバリデーション

--- src/items/dto/create-items.dto.ts
+++ src/items/dto/create-items.dto.ts
@@ -1,8 +1,27 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+// import { IsInt, IsNotEmpty, IsOptional, IsString, MaxLength, Min, IsOptional } from 'class-validator';
+import { IsString, IsNotEmpty, MaxLength, IsInt, Min } from 'class-validator';
+import { IsOptional } from 'class-validator';
+
 export class CreateItemDto {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsString()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsNotEmpty()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @MaxLength(40)
   name: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsInt()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @Min(1)
   price: number;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsOptional()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsString()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @MaxLength(1000)
   description?: string;
 }
