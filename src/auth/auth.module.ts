735f1cf07fa1edf43c74998157f5fe1fbebd7742
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-14 20:23:41 +0900

    保存

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,9 @@ import { JwtModule } from '@nestjs/jwt';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+      signOptions: {
+        expiresIn: '1h',
+      },
     }),
   ],
   controllers: [AuthController],
