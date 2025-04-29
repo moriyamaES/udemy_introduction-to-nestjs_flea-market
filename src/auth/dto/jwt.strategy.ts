8f4b8cd917b94e33e6c5c739604050d3f82ab98e
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-30 01:16:07 +0900

    講師の指示に従い修正

--- src/auth/dto/jwt.strategy.ts
+++ src/auth/dto/jwt.strategy.ts
@@ -11,10 +11,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: process.env.JWT_SECRET!,
     });
   }
-  async validate(payload: JwtPayload): Promise<RequestUser> {
+  validate(payload: JwtPayload): RequestUser {
     return {
       id: payload.sub,
       name: payload.username,
